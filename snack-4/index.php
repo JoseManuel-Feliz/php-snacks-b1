1eb08d9f4f2227ae33f2c7929e2e84764182fa29
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-10-07 02:56:27 +0200

    comment previous filter, added form to filter students grade

--- snack-4/index.php
+++ snack-4/index.php
@@ -1,5 +1,10 @@
 <?php include  __DIR__ . "/partials/vars.php";
+
+/*  
+! previous filter
 $sufficient_grade = 6;
+*/
+$filter_grade = $_GET['grade']
 ?>
 <! DOCTYPE html>
     <html lang="en">
@@ -11,9 +16,18 @@ $sufficient_grade = 6;
     </head>
 
     <body>
+
+        <header>
+            <div>
+                <form action="index.php" method="get">
+                    <input type="number" name="grade" id="grade">
+                </form>
+                <?php !empty($filter_grade)&&isset($filter_grade) ? $filter_grade : '10'?>
+                <?////= $grade?>
+            </div>
+        </header>
         <main>
             <ul>
-
                 <?php foreach ($classi as $classroom => $class) { ?>
                 <li>
                     <?= $classroom ?>
@@ -21,7 +35,7 @@ $sufficient_grade = 6;
                     <?php foreach ($class as $key => $students) { ?>
 
                     <ul>
-                        <?php if ($students['voto_medio'] > $sufficient_grade) { ?>
+                        <?php if ($students['voto_medio'] >= $filter_grade) { ?>
                         <li>
                             <p><?= 'Matricola Nr. :' . $students['id'] ?></p>
                             <p><?= 'Nome: ' . $students['nome'] ?></p>
@@ -39,9 +53,39 @@ $sufficient_grade = 6;
 
                 </li>
                 <?php } ?>
-
-            </ul>
         </main>
+
+        /*
+        !previous filter
+        */
+        <!-- <ul>
+                    <?php //foreach ($classi as $classroom => $class) { ?>
+                    <li>
+                        <?//=$classroom ?>
+
+                        <?php //foreach ($class as $key => $students) { ?>
+
+                        <ul>
+                            <?php //if ($students['voto_medio'] > $sufficient_grade) { ?>
+                            <li>
+                                <p><?//='Matricola Nr. :' . $students['id'] ?></p>
+                                <p><?//='Nome: ' . $students['nome'] ?></p>
+                                <p><?//='Cognome: ' . $students['cognome'] ?></p>
+                                <p><?//='Età: ' . $students['anni'] ?></p>
+                                <p><?//='Voto: ' . $students['voto_medio'] ?></p>
+                                <p><?//='Linguaggio preferito: ' . $students['linguaggio_preferito'] ?></p>
+                                <div><img
+                                        <?//='src="' . $students['immagine'] . '"' . 'alt="Foto di ' . $students['nome'] . ' ' . $students['cognome'] . '"' ?>>
+                                </div>
+                            </li>
+                            <?php //} ?>
+                        </ul>
+                        <?php //} ?>
+
+                    </li>
+                    <?php //} ?>
+
+                </ul>-->
     </body>
 
     </html>
